a31fe9d32e6ba1811b90a1cf6b7f408f17780898
root <root@STJARN11.BME.com>
2022-06-27 12:43:48 +0200

    added test spark app

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,20 +1,27 @@
 package org.example
 
-
 import org.apache.spark.sql.SparkSession
+import org.slf4j.{Logger, LoggerFactory}
+
 /**
  * @author ${user.name}
  */
 object App {
 
-  def foo(x : Array[String]) ={
-    x.foldLeft("")((a,b) => a + b)
+  def foo(x: Array[String]) = {
+    x.foldLeft("")((a, b) => a + b)
 
   }
-  
-  def main(args : Array[String]) {
-    println( "Hello World from Spark!" )
+
+  def main(args: Array[String]) {
+    val logger: Logger = LoggerFactory.getLogger(this.getClass)
+    println("Hello World from Spark!")
     println("concat arguments = " + foo(args))
-    val spark:SparkSession = SparkSession.builder().appName("HelloWorld from Spark").getOrCreate()
+    val spark: SparkSession = SparkSession.builder().appName("HelloWorld from Spark").getOrCreate()
+
+    val testDF = spark.range(1, 100).toDF("columnId")
+    testDF.show()
+    logger.info("Finished Spark Test execution")
+
   }
 }
